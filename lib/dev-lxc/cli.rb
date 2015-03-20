8be5b98764a071ea18db8508f2796e09eaaf0860
Jeremiah Snapp <jeremiah@getchef.com>
2015-03-20 06:00:08 -0400

    Minor improvement to `status` subcommand output

--- lib/dev-lxc/cli.rb
+++ lib/dev-lxc/cli.rb
@@ -52,7 +52,7 @@ module DevLXC::CLI
       raise "A cluster config file must be provided" if config.nil?
       cluster_config = YAML.load(IO.read(config))
 
-      puts "Cluster is available at https://#{cluster_config['api_fqdn']}"
+      puts "Chef Server is available at https://#{cluster_config['api_fqdn']}"
       puts "Analytics is available at https://#{cluster_config['analytics_fqdn']}" if cluster_config['analytics_fqdn']
       match_pattern(pattern).each { |cs| cs.status }
     end
