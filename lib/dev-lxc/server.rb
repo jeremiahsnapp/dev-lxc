c968f015bfbe52467bbe434f749b22a42dfa2e9f
Jeremiah Snapp <jeremiah@chef.io>
2016-06-17 11:16:37 -0400

    Rename @server to @container

--- lib/dev-lxc/server.rb
+++ lib/dev-lxc/server.rb
@@ -3,10 +3,10 @@ require "dev-lxc/container"
 
 module DevLXC
   class Server
-    attr_reader :server
+    attr_reader :container
 
     def initialize(name, ipaddress, additional_fqdn, mounts, ssh_keys)
-      @server = DevLXC::Container.new(name)
+      @container = DevLXC::Container.new(name)
       @ipaddress = ipaddress
       @additional_fqdn = additional_fqdn
       @mounts = mounts
@@ -14,88 +14,88 @@ module DevLXC
     end
 
     def realpath(rootfs_path)
-      "#{@server.config_item('lxc.rootfs')}#{rootfs_path}" if @server.defined?
+      "#{@container.config_item('lxc.rootfs')}#{rootfs_path}" if @container.defined?
     end
 
     def run_command(command)
-      if @server.running?
-        puts "Running '#{command}' in '#{@server.name}'"
-        @server.run_command(command)
+      if @container.running?
+        puts "Running '#{command}' in '#{@container.name}'"
+        @container.run_command(command)
       else
-        puts "'#{@server.name}' is not running"
+        puts "'#{@container.name}' is not running"
       end
     end
 
     def start
-      hwaddr = @server.config_item("lxc.network.0.hwaddr")
-      DevLXC.assign_ip_address(@ipaddress, @server.name, hwaddr)
-      DevLXC.create_dns_record(@additional_fqdn, @server.name, @ipaddress) unless @additional_fqdn.nil?
-      @server.sync_mounts(@mounts)
-      @server.start
-      @server.sync_ssh_keys(@ssh_keys)
+      hwaddr = @container.config_item("lxc.network.0.hwaddr")
+      DevLXC.assign_ip_address(@ipaddress, @container.name, hwaddr)
+      DevLXC.create_dns_record(@additional_fqdn, @container.name, @ipaddress) unless @additional_fqdn.nil?
+      @container.sync_mounts(@mounts)
+      @container.start
+      @container.sync_ssh_keys(@ssh_keys)
     end
 
     def stop
-      hwaddr = @server.config_item("lxc.network.0.hwaddr") if @server.defined?
-      @server.stop
+      hwaddr = @container.config_item("lxc.network.0.hwaddr") if @container.defined?
+      @container.stop
       deregister_from_dnsmasq(hwaddr)
     end
 
     def snapshot(comment=nil)
-      unless @server.defined?
-        puts "WARNING: Skipping snapshot of '#{@server.name}' because it does not exist"
+      unless @container.defined?
+        puts "WARNING: Skipping snapshot of '#{@container.name}' because it does not exist"
         return
       end
-      if @server.running?
-        puts "WARNING: Skipping snapshot of '#{@server.name}' because it is running"
+      if @container.running?
+        puts "WARNING: Skipping snapshot of '#{@container.name}' because it is running"
         return
       end
-      puts "Creating snapshot of container '#{@server.name}'"
-      snapname = @server.snapshot
+      puts "Creating snapshot of container '#{@container.name}'"
+      snapname = @container.snapshot
       unless comment.nil?
-        snapshot = @server.snapshot_list.select { |sn| sn.first == snapname }
+        snapshot = @container.snapshot_list.select { |sn| sn.first == snapname }
         snapshot_comment_file = snapshot.flatten[1]
         IO.write(snapshot_comment_file, comment) unless snapshot_comment_file.nil?
       end
     end
 
     def snapshot_destroy(snapname=nil)
-      unless @server.defined?
-        puts "Skipping container '#{@server.name}' because it does not exist"
+      unless @container.defined?
+        puts "Skipping container '#{@container.name}' because it does not exist"
         return
       end
       if snapname == "ALL"
-        if @server.snapshot_list.empty?
-          puts "Container '#{@server.name}' does not have any snapshots"
+        if @container.snapshot_list.empty?
+          puts "Container '#{@container.name}' does not have any snapshots"
         else
-          @server.snapshot_list.each do |snapshot|
-            puts "Destroying snapshot '#{snapshot.first}' of container '#{@server.name}'"
-            @server.snapshot_destroy(snapshot.first)
+          @container.snapshot_list.each do |snapshot|
+            puts "Destroying snapshot '#{snapshot.first}' of container '#{@container.name}'"
+            @container.snapshot_destroy(snapshot.first)
           end
         end
       elsif snapname == "LAST"
-        if @server.snapshot_list.empty?
-          puts "Container '#{@server.name}' does not have any snapshots"
+        if @container.snapshot_list.empty?
+          puts "Container '#{@container.name}' does not have any snapshots"
         else
-          snapname = @server.snapshot_list.last.first
-          puts "Destroying snapshot '#{snapname}' of container '#{@server.name}'"
-          @server.snapshot_destroy(snapname)
+          snapname = @container.snapshot_list.last.first
+          puts "Destroying snapshot '#{snapname}' of container '#{@container.name}'"
+          @container.snapshot_destroy(snapname)
         end
       else
-        snapshot = @server.snapshot_list.select { |sn| sn.first == snapname }
+        snapshot = @container.snapshot_list.select { |sn| sn.first == snapname }
         if snapshot.flatten.empty?
-          puts "Container '#{@server.name}' does not have a '#{snapname}' snapshot"
+          puts "Container '#{@container.name}' does not have a '#{snapname}' snapshot"
         else
-          puts "Destroying snapshot '#{snapname}' of container '#{@server.name}'"
-          @server.snapshot_destroy(snapname)
+          puts "Destroying snapshot '#{snapname}' of container '#{@container.name}'"
+          @container.snapshot_destroy(snapname)
         end
       end
     end
 
     def snapshot_list
       snapshots = Array.new
-      return snapshots unless @server.defined?
-      @server.snapshot_list.each do |snapshot|
+      return snapshots unless @container.defined?
+      @container.snapshot_list.each do |snapshot|
         (snapname, snap_comment_file, snaptime) = snapshot
         snap_comment = IO.read(snap_comment_file).chomp if File.exist?(snap_comment_file)
         snapshots << [snapname, snaptime, snap_comment]
@@ -104,39 +104,39 @@ module DevLXC
     end
 
     def snapshot_restore(snapname=nil)
-      unless @server.defined?
-        puts "WARNING: Skipping container '#{@server.name}' because it does not exist"
+      unless @container.defined?
+        puts "WARNING: Skipping container '#{@container.name}' because it does not exist"
         return
       end
-      if @server.running?
-        puts "WARNING: Skipping container '#{@server.name}' because it is running"
+      if @container.running?
+        puts "WARNING: Skipping container '#{@container.name}' because it is running"
         return
       end
       if snapname == "LAST"
-        if @server.snapshot_list.empty?
-          puts "WARNING: Skipping container '#{@server.name}' because it does not have any snapshots"
+        if @container.snapshot_list.empty?
+          puts "WARNING: Skipping container '#{@container.name}' because it does not have any snapshots"
         else
-          snapname = @server.snapshot_list.last.first
-          puts "Restoring snapshot '#{snapname}' of container '#{@server.name}'"
-          @server.snapshot_restore(snapname)
+          snapname = @container.snapshot_list.last.first
+          puts "Restoring snapshot '#{snapname}' of container '#{@container.name}'"
+          @container.snapshot_restore(snapname)
         end
       else
-        snapshot = @server.snapshot_list.select { |sn| sn.first == snapname }
+        snapshot = @container.snapshot_list.select { |sn| sn.first == snapname }
         if snapshot.flatten.empty?
-          puts "WARNING: Skipping container '#{@server.name}' because it does not have a '#{snapname}' snapshot"
+          puts "WARNING: Skipping container '#{@container.name}' because it does not have a '#{snapname}' snapshot"
         else
-          puts "Restoring snapshot '#{snapname}' of container '#{@server.name}'"
-          @server.snapshot_restore(snapname)
+          puts "Restoring snapshot '#{snapname}' of container '#{@container.name}'"
+          @container.snapshot_restore(snapname)
         end
       end
     end
 
     def destroy
-      if @server.defined?
-        hwaddr = @server.config_item("lxc.network.0.hwaddr")
-        @server.snapshot_list.each { |snapshot| @server.snapshot_destroy(snapshot.first) }
+      if @container.defined?
+        hwaddr = @container.config_item("lxc.network.0.hwaddr")
+        @container.snapshot_list.each { |snapshot| @container.snapshot_destroy(snapshot.first) }
       end
-      @server.destroy
+      @container.destroy
       deregister_from_dnsmasq(hwaddr)
     end
 
